17b4a07c92ddd1616143061a72528e92e7b8d95a
alexpaden <padenalex0@gmail.com>
2024-06-27 14:05:13 -0600

    add /user endpoint
    running on neynar + airstack, will be replaced

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,18 @@ import * as express from "express";
 import * as functions from "firebase-functions";
 import threadRoutes from "./thread";
 import channelRoutes from "./channel";
+import userRoutes from "./user";
 
 const app = express();
 app.use(express.json());
 
 app.use("/thread", threadRoutes);
 app.use("/channel", channelRoutes);
+app.use("/user", userRoutes);
 
-exports.api = functions.https.onRequest(app);
+exports.api = functions
+  .runWith({
+    timeoutSeconds: 300,
+    memory: '512MB'
+  })
+  .https.onRequest(app);
